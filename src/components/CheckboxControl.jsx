8e421321175bfde55ded3f7dd83e442d01eda30d
Alexandru Pomohaci <alexandrupomohaci@yahoo.com>
2018-12-01 17:02:57 +0200

    Implemented checkboxControl component with fixed tooltip message

---
+++ src/components/CheckboxControl.jsx
@@ -0,0 +1,51 @@
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import FontAwesome from "react-fontawesome";
+import "../styles/CheckboxControl.css";
+
+export default class CheckboxControl extends Component {
+  constructor(props) {
+    super(props);
+    this.handleChange = this.handleChange.bind(this);
+
+    this.state = {
+      checked: false
+    };
+  }
+
+  handleChange(e) {
+    const checked = !this.state.checked;
+    this.setState({ checked });
+    this.props.onChange();
+  }
+
+  render() {
+    var checkboxIcon;
+    if (this.state.checked) checkboxIcon = "fas fa-check-square";
+    else checkboxIcon = "fas fa-square";
+    return (
+      <div className="checkbox-control-container">
+        <div className="checkbox-container">
+          <FontAwesome
+            className="checkbox-icon"
+            name={checkboxIcon}
+            onClick={this.handleChange}
+          />
+          <span className="label-text">{this.props.label}</span>
+          <FontAwesome
+            className="tooltip-icon"
+            name="fas fa-info-circle"
+            onClick={this.handleChange}
+          />
+          <span className="tooltip-text">{this.props.tooltipText}</span>
+        </div>
+      </div>
+    );
+  }
+}
+
+CheckboxControl.propTypes = {
+  onChange: PropTypes.func.isRequired,
+  label: PropTypes.string.isRequired,
+  tooltipText: PropTypes.string.isRequired
+};
