60779fab993d48a3bc9390a0e35502dbd3618bfc
Alexandru Pomohaci <alexandrupomohaci@yahoo.com>
2018-12-01 18:45:48 +0200

    Format changes

--- src/components/CheckboxControl.jsx
+++ src/components/CheckboxControl.jsx
@@ -4,50 +4,56 @@ import FontAwesome from "react-fontawesome";
 import "../styles/CheckboxControl.css";
 
 export default class CheckboxControl extends Component {
-  constructor(props) {
-    super(props);
-    this.handleChange = this.handleChange.bind(this);
+    constructor(props) {
+        super(props);
+        this.handleChange = this.handleChange.bind(this);
 
-    this.state = {
-      checked: props.defaultValue === undefined ? false : props.defaultValue
-    };
-  }
+        this.state = {
+            checked:
+                props.defaultValue === undefined ? false : props.defaultValue
+        };
+    }
 
-  handleChange(e) {
-    const checked = !this.state.checked;
-    this.setState({ checked });
-    this.props.onChange(checked);
-  }
+    handleChange(e) {
+        const checked = !this.state.checked;
+        this.setState({ checked });
+        this.props.onChange(checked);
+    }
 
-  render() {
-    var checkboxIcon;
-    if (this.state.checked) checkboxIcon = "fas fa-check-square";
-    else checkboxIcon = "fas fa-square";
+    render() {
+        var checkboxIcon;
+        if (this.state.checked) checkboxIcon = "fas fa-check-square";
+        else checkboxIcon = "fas fa-square";
 
-    return (
-      <div className="checkbox-control-container">
-        <div className="checkbox-container">
-          <FontAwesome
-            className="checkbox-icon"
-            name={checkboxIcon}
-            onClick={this.handleChange}
-          />
-          <span className="label-text">{this.props.label}</span>
-          {this.props.tooltipText && (
-            <React.Fragment>
-              <FontAwesome className="tooltip-icon" name="fas fa-info-circle" />
-              <span className="tooltip-text">{this.props.tooltipText}</span>
-            </React.Fragment>
-          )}
-        </div>
-      </div>
-    );
-  }
+        return (
+            <div className="checkbox-control-container">
+                <div className="checkbox-container">
+                    <FontAwesome
+                        className="checkbox-icon"
+                        name={checkboxIcon}
+                        onClick={this.handleChange}
+                    />
+                    <span className="label-text">{this.props.label}</span>
+                    {this.props.tooltipText && (
+                        <React.Fragment>
+                            <FontAwesome
+                                className="tooltip-icon"
+                                name="fas fa-info-circle"
+                            />
+                            <span className="tooltip-text">
+                                {this.props.tooltipText}
+                            </span>
+                        </React.Fragment>
+                    )}
+                </div>
+            </div>
+        );
+    }
 }
 
 CheckboxControl.propTypes = {
-  onChange: PropTypes.func.isRequired,
-  label: PropTypes.string.isRequired,
-  defaultValue: PropTypes.bool,
-  tooltipText: PropTypes.string
+    onChange: PropTypes.func.isRequired,
+    label: PropTypes.string.isRequired,
+    defaultValue: PropTypes.bool,
+    tooltipText: PropTypes.string
 };
--- src/components/SliderControl.jsx
+++ src/components/SliderControl.jsx
@@ -1,70 +1,75 @@
-import React, { Component } from "react";
-import PropTypes from "prop-types";
-import FontAwesome from "react-fontawesome";
-import "../styles/SliderControl.css";
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+import FontAwesome from 'react-fontawesome'
+import '../styles/SliderControl.css'
 
 export default class SliderControl extends Component {
-  constructor(props) {
-    super(props);
-    this.handleChange = this.handleChange.bind(this);
-    let defaultValue = this.props.defaultValue;
+    constructor(props){
+        super(props);
+        this.handleChange = this.handleChange.bind(this);
+        let defaultValue = this.props.defaultValue;
+        
+        if(!defaultValue)
+            defaultValue = (this.props.maxValue + this.props.minValue) / 2;
+        
+        this.state = {
+            value: defaultValue
+        };
+    }
 
-    if (!defaultValue)
-      defaultValue = (this.props.maxValue + this.props.minValue) / 2;
+    handleChange(e){
+        const value = e.target.value;
+        this.setState({value})
+        this.props.onChange(value);
+    }
+	render() {
 
-    this.state = {
-      value: defaultValue
-    };
-  }
-
-  handleChange(e) {
-    const value = e.target.value;
-    this.setState({ value });
-    this.props.onChange(value);
-  }
-  
-  render() {
-    return (
-      <div className="slider-control-container">
-        <div className="label-container">
-          <span className="label-icon">
-            <FontAwesome name={this.props.iconName} />
-          </span>
-          <span className="label-text">{this.props.label}:</span>
-          <span className="value-input">
-            <input
-              type="text"
-              value={this.state.value}
-              onChange={this.handleChange}
-              style={{
-                width: (String(this.props.maxValue).length + 2) * 0.7 + "rem"
-              }}
-            />
-          </span>
-          {this.props.unit && <span className="unit">{this.props.unit}</span>}
-        </div>
-        <div className="range-container">
-          <input
-            type="range"
-            min={this.props.minValue}
-            max={this.props.maxValue}
-            value={this.state.value}
-            onChange={this.handleChange}
-            step={this.props.step}
-          />
-        </div>
-      </div>
-    );
-  }
+		return (
+			<div className="slider-control-container">
+				<div className="label-container">
+                    <span className="label-icon">
+                        <FontAwesome name={this.props.iconName} /> 
+                    </span>
+                    <span className="label-text">
+                        {this.props.label}:
+                    </span>
+                    <span className="value-input">
+                        <input
+                            type="text"
+                            value={this.state.value}
+                            onChange={this.handleChange}
+                            style={{
+                                width: (String(this.props.maxValue).length+2) * .7 + 'rem'
+                            }}
+                        />
+                    </span>
+                    {this.props.unit && (
+                        <span className="unit">{this.props.unit}</span>
+                    )}
+                </div>
+                <div className="range-container">
+                    <input
+                        type="range"
+                        min={this.props.minValue}
+                        max={this.props.maxValue}
+                        value={this.state.value}
+                        onChange={this.handleChange}
+                        step={this.props.step}
+                    />
+                </div>
+			</div>
+		)
+    }
+    
 }
 
 SliderControl.propTypes = {
-  iconName: PropTypes.string.isRequired,
-  onChange: PropTypes.func.isRequired,
-  label: PropTypes.string.isRequired,
-  unit: PropTypes.string,
-  minValue: PropTypes.number.isRequired,
-  maxValue: PropTypes.number.isRequired,
-  defaultValue: PropTypes.number,
-  step: PropTypes.number.isRequired
-};
+    iconName: PropTypes.string.isRequired,
+    onChange: PropTypes.func.isRequired,
+    label: PropTypes.string.isRequired,
+    unit: PropTypes.string,
+    minValue: PropTypes.number.isRequired,
+    maxValue: PropTypes.number.isRequired,
+    defaultValue: PropTypes.number,
+    step: PropTypes.number.isRequired
+}
