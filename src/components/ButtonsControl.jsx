fbfa70b0277b5aa574e0d4ce267f19993953378d
Gheorghe Aleandru Constantin <alexandru.gheorghe@info.uaic.ro>
2018-12-07 21:16:07 +0200

    ButtonsControl refăcut să primească datele prin props

--- src/components/ButtonsControl.jsx
+++ src/components/ButtonsControl.jsx
@@ -4,31 +4,35 @@ import FontAwesome from 'react-fontawesome'
 import PropTypes from 'prop-types'
 import ButtonContainer from './ButtonContainer';
 
-const ButtonsControl = ({handleBack, handleAdjust, handleUpload})=>(
+const ButtonsControl = ({handlePrimary , handleSecondary , handleAuxiliary , primaryLabel , secondaryLabel , auxiliaryLabel , iconLabel})=>(
         <div className="buttons-group">
             <ButtonContainer 
-                label="Back" 
-                icon={<FontAwesome className="fas fa-undo-alt icon-back" name="back" />}
-                handleClick={handleBack}
+                label={primaryLabel} 
+                icon={<FontAwesome className={iconLabel} name="back" />}
+                handleClick={handlePrimary}
                 className="primary-button"
             />
             <ButtonContainer 
-                label="Adjust image" 
-                handleClick={handleAdjust}
+                label={secondaryLabel}
+                handleClick={handleSecondary}
                 className="secondary-button"
             />
             <ButtonContainer 
-                label="Upload other image" 
-                handleClick={handleUpload}
+                label={auxiliaryLabel} 
+                handleClick={handleAuxiliary}
                 className="auxiliary-button"
             />
         </div>
 )
 
 ButtonsControl.propTypes = {
-    handleBack:PropTypes.func.isRequired,
-    handleAdjust:PropTypes.func.isRequired,
-    handleUpload:PropTypes.func.isRequired,
+    handlePrimary:PropTypes.func.isRequired,
+    handleSecondary:PropTypes.func.isRequired,
+    handleAuxiliary:PropTypes.func.isRequired,
+    primaryLabel:PropTypes.string.isRequired,
+    secondaryLabel:PropTypes.string.isRequired,
+    auxiliaryLabel:PropTypes.string.isRequired,
+    iconLabel:PropTypes.string.isRequired
 }
 
 export default ButtonsControl;
