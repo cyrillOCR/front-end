c411ff57992f498331d1f4de1e14ca680fad0b39
Nohai Bogdan <nohai.ionut02@gmail.com>
2019-01-10 16:30:26 +0200

    Add new sliders and checkboxes

--- src/components/SegmentationScreen.jsx
+++ src/components/SegmentationScreen.jsx
@@ -13,23 +13,53 @@ class SegmentationScreen extends Component {
         super(props);
 
         this.propsForSidekick = {
-            title: `Finding letters...`
+            title: 'Finding letters...'
         }
 
-        this.propsForThresholdSlider = {
-            maxValue: 200,
-            minValue: 0,
-            defaultValue: 100,
+        this.propsForContrastFactorSlider = {
+            maxValue: 3,
+            minValue: 1,
+            defaultValue: 1.5,
+            step: 0.01,
             iconName: 'fas fa-adjust',
-            label: 'Threshold',
-            unit: '',
-            step: 1
+            label: 'Contrast'
         }
 
-        this.propsForDilationCheckbox = {
+        this.propsForSegmentationFactorSlider = {
+            maxValue: 0.7,
+            minValue: 0.3,
+            defaultValue: 0.5,
+            step: 0.01,
+            iconName: 'fas fa-adjust',
+            label: 'Segmentaion'
+        }
+
+        this.propsForSeparationFactorSlider = {
+            maxValue: 8,
+            minValue: 2,
+            defaultValue: 3,
+            step: 1,
+            unit: 'px',
+            iconName: 'fas fa-adjust',
+            label: 'Separation'
+        }
+
+        this.propsForApplyDilationCheckbox = {
+            defaultValue: true,
+            label: 'Dilation',
+            toolTipText: 
+                `Dilation can be true or false, by default is true. 
+                If set, it dilates and erosions the characters, making them more clear,
+                but has very long execution time.`
+        }
+
+        this.propsForApplyNoiseReductionCheckbox = {
             defaultValue: false,
-            label: `Dilation + Erosion`,
-            toolTipText: 'daw'
+            label: 'Noise Reduction',
+            toolTipText: 
+                `NoiseReduction can be false or true, by default it is false. 
+                If set, it totally removes the noise of the image, making it 
+                sharp and clear but increases the execution time.`
         }
 
         this.propsForButtonsControl = {
@@ -50,8 +80,11 @@ class SegmentationScreen extends Component {
             handleSecondary, 
             handleAuxiliary,
             onChangePage,
-            onChangeDilation,
-            onChangeThreshold
+            onChangeApplyDilation,
+            onChangeApplyNoiseReduction,
+            onChangeContrastFactor,
+            onChangeSegmentationFactor,
+            onChangeSeparationFactor
         } = this.props; 
         var imagePreview = null;
         
@@ -72,21 +105,55 @@ class SegmentationScreen extends Component {
                         totalPages={totalPages}>
                         <div className="slider-control">
                             <SliderControl
-                                maxValue={this.propsForThresholdSlider.maxValue} 
-                                minValue={this.propsForThresholdSlider.minValue}
-                                defaultValue={this.propsForThresholdSlider.defaultValue} 
-                                iconName={this.propsForThresholdSlider.iconName} 
-                                label={this.propsForThresholdSlider.label} 
-                                unit={this.propsForThresholdSlider.unit}
-                                step={this.propsForThresholdSlider.step} 
-                                onChange={onChangeThreshold} />
+                                maxValue={this.propsForContrastFactorSlider.maxValue} 
+                                minValue={this.propsForContrastFactorSlider.minValue}
+                                defaultValue={this.propsForContrastFactorSlider.defaultValue} 
+                                iconName={this.propsForContrastFactorSlider.iconName} 
+                                label={this.propsForContrastFactorSlider.label} 
+                                unit={this.propsForContrastFactorSlider.unit}
+                                step={this.propsForContrastFactorSlider.step} 
+                                onChange={onChangeContrastFactor} />
                         </div>
+
+                        <div className="slider-control">
+                            <SliderControl
+                                maxValue={this.propsForSegmentationFactorSlider.maxValue} 
+                                minValue={this.propsForSegmentationFactorSlider.minValue}
+                                defaultValue={this.propsForSegmentationFactorSlider.defaultValue} 
+                                iconName={this.propsForSegmentationFactorSlider.iconName} 
+                                label={this.propsForSegmentationFactorSlider.label} 
+                                unit={this.propsForSegmentationFactorSlider.unit}
+                                step={this.propsForSegmentationFactorSlider.step} 
+                                onChange={onChangeSegmentationFactor} />
+                        </div>
+
+                        <div className="slider-control">
+                            <SliderControl
+                                maxValue={this.propsForSeparationFactorSlider.maxValue} 
+                                minValue={this.propsForSeparationFactorSlider.minValue}
+                                defaultValue={this.propsForSeparationFactorSlider.defaultValue} 
+                                iconName={this.propsForSeparationFactorSlider.iconName} 
+                                label={this.propsForSeparationFactorSlider.label} 
+                                unit={this.propsForSeparationFactorSlider.unit}
+                                step={this.propsForSeparationFactorSlider.step} 
+                                onChange={onChangeSeparationFactor} />
+                        </div>
+
+
                         <div className="checkbox-control">
                             <CheckboxControl
-                                defaultValue={this.propsForDilationCheckbox.defaultValue}
-                                label={this.propsForDilationCheckbox.label}
-                                tooltipText={this.propsForDilationCheckbox.toolTipText}
-                                onChange={onChangeDilation} />
+                                defaultValue={this.propsForApplyDilationCheckbox.defaultValue}
+                                label={this.propsForApplyDilationCheckbox.label}
+                                tooltipText={this.propsForApplyDilationCheckbox.toolTipText}
+                                onChange={onChangeApplyDilation} />
+                        </div>
+
+                        <div className="checkbox-control">
+                            <CheckboxControl
+                                defaultValue={this.propsForApplyNoiseReductionCheckbox.defaultValue}
+                                label={this.propsForApplyNoiseReductionCheckbox.label}
+                                tooltipText={this.propsForApplyNoiseReductionCheckbox.toolTipText}
+                                onChange={onChangeApplyNoiseReduction} />
                         </div>
                         
                         <div className="buttons-control">
@@ -116,7 +183,10 @@ SegmentationScreen.propTypes = {
     handleAuxiliary: PropTypes.func.isRequired,
     onChangePage: PropTypes.func.isRequired,
     onChangeDilation: PropTypes.func.isRequired,
-    onChangeThreshold: PropTypes.func.isRequired,
+    onChangeNoiseReduction: PropTypes.func.isRequired,
+    onChangeContrastFactor: PropTypes.func.isRequired,
+    onChangeSegmentationFactor: PropTypes.func.isRequired,
+    onChangeSeparationFactor: PropTypes.func.isRequired
 }
 
 export default SegmentationScreen;
