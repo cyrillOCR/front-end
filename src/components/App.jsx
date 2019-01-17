e1df8765120141dfa0edf84f64f3cda55983419f
Hasna Cristian <cristian.hasna@info.uaic.ro>
2019-01-18 01:08:27 +0200

    Back to square 0

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -157,7 +157,7 @@ class App extends Component {
         }
       })
     }
-    return axios.post("https://golar3.go.ro:5000/addImage", {
+    return axios.post("http://golar3.go.ro:5000/addImage", {
       // return axios.post("http://localhost:5000/addImage", {
       name: "image.jpg",
       payload: imageURI.substr(imageURI.indexOf(",") + 1),
@@ -204,7 +204,7 @@ class App extends Component {
       })
     }
     // return Promise.resolve({ boxes: [], text: args.imageURI })
-    return axios.post('https://golar3.go.ro:5050/feature', {
+    return axios.post('http://golar3.go.ro:5050/feature', {
       // return axios.post('http://192.168.0.103:5050/feature', {
       coords: coords,
       base64: processedImage
