0e92decc1412cf8b995f28f3ca352751301fd96d
Hasna Cristian <cristian.hasna@info.uaic.ro>
2019-01-26 16:04:28 +0200

    https links update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -114,7 +114,7 @@ class App extends Component {
         return Promise.resolve([docURI]);
       }
       //post request to addPdf if doc is PDF
-      return axios.post('https://pre-cyrill.fwd.wf/convertPdf', {
+      return axios.post('https://neco.serveo.net/convertPdf', {
         // return axios.post('http://localhost:5000/convertPdf', {
         name: "doc.pdf",
         payload: docURI.split(',')[1]
@@ -157,7 +157,7 @@ class App extends Component {
         }
       })
     }
-    return axios.post("https://pre-cyrill.fwd.wf/addImage", {
+    return axios.post("https://neco.serveo.net/addImage", {
       // return axios.post("http://localhost:5000/addImage", {
       name: "image.jpg",
       payload: imageURI.substr(imageURI.indexOf(",") + 1),
@@ -204,7 +204,7 @@ class App extends Component {
       })
     }
     // return Promise.resolve({ boxes: [], text: args.imageURI })
-    return axios.post('https://fex-cyrill.fwd.wf/feature', {
+    return axios.post('https://miscui.serveo.net/feature', {
       // return axios.post('http://192.168.0.103:5050/feature', {
       coords: coords,
       base64: processedImage
